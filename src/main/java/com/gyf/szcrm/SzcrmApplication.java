84cbadfd3c6e907058176ed8d6ab09235d71f928
root <964564277@qq.com>
2020-08-26 18:31:25 +0800

    Initial commit

---
+++ src/main/java/com/gyf/szcrm/SzcrmApplication.java
@@ -0,0 +1,13 @@
+package com.gyf.szcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzcrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzcrmApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gyf/szcrm/web/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.gyf.szcrm.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
